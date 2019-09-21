d924137d85f8d3cf3ca7b4a4da01217445312287
Sean Middleditch <sean.middleditch@gmail.com>
2019-09-21 16:18:53 -0700

    setup-ninja

---
+++ setup-ninja/index.js
@@ -0,0 +1,62 @@
+const core = require('@actions/core')
+const process = require('process')
+const path = require('path')
+const fs = require('fs')
+const URL = require('url').URL
+const { https } = require('follow-redirects')
+const AdmZip = require('adm-zip')
+
+const selectPlatforn = (platform) =>
+    platform ? [null, platform] :
+    process.platform === 'win32' ? [null, 'win'] :
+    process.platform === 'darwin' ? [null, 'mac'] :
+    process.platform === 'linux' ? [null, 'linux'] :
+    [new Error(`Unsupported platform '${process.platform}'`), '']
+
+try {
+    const version = core.getInput('version', {required: true})
+    const destDir = core.getInput('destination') || 'ninja-build'
+
+    const [error, platform] = selectPlatforn(core.getInput('platform'));
+    if (error) throw error
+
+    const url = new URL(`https://github.com/ninja-build/ninja/releases/download/v${version}/ninja-${platform}.zip`)
+    //const url = new URL('https://google.com')
+    console.log(`downloading ${url}`)
+
+    const request = https.get(url, {followAllRedirects: true}, result => {
+        const data = []
+
+        result.on('data', chunk => data.push(chunk))
+
+        result.on('end', () => {
+            const length = data.reduce((len, chunk) => len + chunk.length, 0)
+            const buffer = Buffer.alloc(length)
+
+            console.log(`received file, size ${buffer.length}`)
+
+            data.reduce((pos, chunk) => {
+                chunk.copy(buffer, pos)
+                return pos + chunk.length
+            }, 0)
+
+            const zip = new AdmZip(buffer)
+            const entry = zip.getEntries()[0]
+
+            const fullDestDir = path.resolve(process.cwd(), destDir)
+            if (!fs.existsSync(fullDestDir)) {
+                fs.mkdirSync(fullDestDir, {recursive: true})
+            }
+
+            const fullFileDir = path.join(fullDestDir, entry.entryName)
+            zip.extractEntryTo(entry.entryName, fullFileDir)
+
+            console.log(`extracted '${entry.entryName}' to '${fullFileDir}'`)
+
+            core.addPath(fullDestDir)
+        })
+    })
+    request.on('error', error => { throw error })
+} catch (error) {
+    core.setFailed(error.message)
+}
